59eb213bea79387945743379b9badd3c6d453a2a
Pingui <08joaovitor01@gmail.com>
2024-05-13 23:38:41 -0300

    File e FileInfo

---
+++ Arquivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Projetos\Flopinguim\csharp-studies\Arquivos\Hello.txt";
+            string targetPath = @"C:\Projetos\Flopinguim\csharp-studies\Arquivos\Hello2.txt";
+
+            try
+            {
+                string[] lines  = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                    Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
