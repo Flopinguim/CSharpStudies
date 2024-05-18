24352d7c73036d1d83691845f80f17d01d8d6931
Pingui <08joaovitor01@gmail.com>
2024-05-18 20:42:23 -0300

    Criando logica para apagar todos os arquivos de uma pasta (teste para uma tarefa do trabalho)

--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Arquivos
 {
@@ -15,12 +16,38 @@ namespace Arquivos
 
             try
             {
-                string[] lines  = File.ReadAllLines(sourcePath);
+                Console.Write("Faça: ");
+                int faca = int.Parse(Console.ReadLine());
 
-                foreach (string line in lines)
-                    Console.WriteLine(line);
+                if (faca.Equals(1))
+                {
+                    string[] lines = File.ReadAllLines(sourcePath);
+
+                    foreach (string line in lines)
+                        Console.WriteLine(line);
+                }
+                else if (faca.Equals(2))
+                {
+                    Console.WriteLine("Digite o caminho da pasta que deseja limpar:");
+                    string pasta = Console.ReadLine();
+
+                    if (Directory.Exists(pasta))
+                    {
+                        string[] arquivos = Directory.GetFiles(pasta);
+
+                        foreach (string arquivo in arquivos)
+                        {
+                            File.Delete(arquivo);
+                            Console.WriteLine($"Arquivo '{arquivo}' deletado com sucesso.");
+                        }
+
+                        Console.WriteLine("Todos os arquivos foram deletados.");
+                    }
+                    else
+                        Console.WriteLine("O caminho da pasta não é válido ou não existe.");
+                }
             }
-            catch (IOException e)
+            catch (Exception e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
